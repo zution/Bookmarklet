2f066a3199004be27578e5c95ee7b3fb476baf0e
zution <98178665+zution@users.noreply.github.com>
2022-02-24 20:32:47 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,7 @@
 /*
-BOOKMARKLET: javascript:(function(){var el=document.createElement('script');el.type='text/javascript';el.src='https://zution.github.io/Bookmarklet/bookmarklet.js';el.onerror=function(){alert("Due to the Content Security Policy, this bookmarklet is unable to run. There is nothing you can do. If you are the owner and are a developer, consider going here: \n\n https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP");};document.getElementsByTagName('body')[0].appendChild(el);})();
+BOOKMARKLET: javascript:(function(){var el=document.createElement('script');el.type='text/javascript';el.src='https://zution.github.io/Bookmarklet/bookmarklet.js';el.onerror=function(){alert("Due to the Content Security Policy, this bookmarklet is unable to run. There is nothing you can do. If you are the owner and are a developer, consider going here: \n\nhttps://developer.mozilla.org/en-US/docs/Web/HTTP/CSP");};document.getElementsByTagName('body')[0].appendChild(el);})();
+
+I feel rusty in my coding skills tbh
 */
 var answer = prompt("What do you need?");
 switch(answer) {
@@ -63,9 +65,10 @@ switch(answer) {
         break;
     case "convert temp":
         var ans = prompt("C to F or F to C?");
-        if (ans == 1 || ans == "C to F" || ans == "one" || ans == "first") {
+        ans = ans.toLowerCase();
+        if (ans == 1 || ans == "c to f" || ans == "one" || ans == "first") {
             alert(Math.round(Number(prompt("temperature?")) * 1.8 + 32) + "° F");
-        } else if (ans == 2 || ans == "F to C" || ans == "two" || ans == "second") {
+        } else if (ans == 2 || ans == "f to c" || ans == "two" || ans == "second") {
             alert(Math.round((prompt("temperature?") - 32) * 5 / 9) + "° C");
         } else {
             alert("Invalid answer.");
@@ -78,8 +81,39 @@ switch(answer) {
         javascript: (function() { var d = document, e = d.getElementById('wappalyzer-container'); if (e !== null) { d.body.removeChild(e); } var u = 'https://www.wappalyzer.com/', t = new Date().getTime(), c = d.createElement('div'), p = d.createElement('div'), l = d.createElement('link'), s = d.createElement('script'); c.setAttribute('id', 'wappalyzer-container'); l.setAttribute('rel', 'stylesheet'); l.setAttribute('href', u + 'css/bookmarklet.css'); d.head.appendChild(l); p.setAttribute('id', 'wappalyzer-pending'); p.setAttribute('style', 'background-image: url(' + u + 'images/spinner.gif) !important'); c.appendChild(p); s.setAttribute('src', u + 'bookmarklet/wappalyzer.js'); s.onload = function() { window.wappalyzer = new Wappalyzer(); s = d.createElement('script'); s.setAttribute('src', u + 'bookmarklet/apps.js'); s.onload = function() { s = d.createElement('script'); s.setAttribute('src', u + 'bookmarklet/driver.js'); c.appendChild(s); }; c.appendChild(s); }; c.appendChild(s); d.body.appendChild(c); })();
         break;
     case "help":
-        alert("calc: supports two number equations, with addition, subtraction, multiplication, division, modulus, and exponents\nsrch: searches google\ndark: enables dark mode\nrevert dark: reverts dark mode\nremove cookies: removes cookies\nshorten url: shortens url with is.gd\nedit page: shows an interface to edit the page\nperformance: shows technical statistics\njoke: tells a joke\nconvert temp: converts C to F or vice versa\nnews: shows news written by me\nusing: shows what the site is using\nhelp: list of commands");
+        alert("calc: supports two number equations, with addition, subtraction, multiplication, division, modulus, and exponents\nsrch: searches google\ndark: enables dark mode\nrevert dark: reverts dark mode\nremove cookies: removes cookies\nqr code: gives you a qr code for the current site, a link, or text\nshorten url: shortens url with is.gd\nedit page: shows an interface to edit the page\nperformance: shows technical statistics\njoke: tells a joke\nconvert temp: converts C to F or vice versa\nnews: shows news written by me\nusing: shows what the site is using\nhelp: list of commands");
+        break;
+    case "qr code":
+        var ans = prompt("The current site, a link, or text?");
+        ans = ans.toLowerCase();
+        function validUrl(str) { // shamelessly copied from stack overflow
+            var pattern = new RegExp('^(https?:\\/\\/)?'+
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+
+            '((\\d{1,3}\\.){3}\\d{1,3}))'+
+            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+
+            '(\\?[;&a-z\\d%_.~+=-]*)?'+
+            '(\\#[-a-z\\d_]*)?$','i');
+            return !!pattern.test(str);
+        }
+        if (ans == "site" || ans == "current site" || ans == "this site" || ans == document.URL) {
+            window.open("https://chart.apis.google.com/chart?cht=qr&chs=500x500&choe=UTF-8&chld=H|0&chl=" + document.URL);
+        } else if (ans == "link" || ans == "a link" || ans.includes("https://") || ans.includes("http://" || ans == "url")) {
+            var link = prompt("What link?");
+            if (validUrl(link) == true) {
+                window.open("https://chart.apis.google.com/chart?cht=qr&chs=500x500&choe=UTF-8&chld=H|0&chl=" + link);
+            } else {
+                alert("Not a valid link. Make sure you include things like \"https://\".");
+            }
+        } else if (ans == "text" || ans == "txt") {
+            window.open("https://chart.apis.google.com/chart?cht=qr&chs=500x500&choe=UTF-8&chld=H|0&chl=" + prompt("What text?"));
+        } else {
+            alert("\"" + ans + "\" is not a valid choice.")
+        }
         break;
     default:
-        alert("Sorry, please enter another command. That command is not recognized.");
+        if (answer !== "") {
+            alert("Sorry, please enter another command. That command is not recognized.");
+        } else {
+            alert("You want me to do nothing? Then I will!");
+        }
 }
